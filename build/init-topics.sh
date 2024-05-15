0bd476d9c32d9a0d62388565b2532766d184b394
Kyle J. Davis <kyledvs@amazon.com>
2024-05-15 07:46:24 -0600

    adds safeguards to init topics script
    Signed-off-by: Kyle J. Davis <kyledvs@amazon.com>

--- build/init-topics.sh
+++ build/init-topics.sh
@@ -2,6 +2,19 @@
 # See README for usage
 # This file will create stub files for all the docs/topics.
 
+# first check to make sure there are arguments
+if [ -z "$1" ]; then
+    echo "You must supply a path to the topics as the first argument" 
+    exit 1
+fi 
+
+# check for validity of these agruments as paths
+if [ ! -d "$1" ]; then
+    echo "The topics directory must exist and be a valid path"
+    exit 1
+fi
+
+
 ln -s $1 ./build-topics 
 
 for fname in $(find $1 -maxdepth 1  -iname "*.md")
